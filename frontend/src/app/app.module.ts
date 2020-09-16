a25d93ca694ca7c6f9adf07fa441eba43fd87527
netomatiazi <netoomn@gmail.com>
2020-09-16 16:03:56 -0300

    Add Header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+// Importando a toobar do Material
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // Referenciando o módulo dentro de imports
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
